6317ad28d9df3b1bba134503690daf34b5d6b450
SunjatunRuna <sunjatuna@gmail.com>
2022-04-05 21:05:35 +0600

    about semantic tag

--- src/components/Blogs/Blogs.js
+++ src/components/Blogs/Blogs.js
@@ -11,6 +11,13 @@ const Blogs = () => {
                     এর মাধ্যমে সহজে ডাটা আদান প্রদান করা যায়।
                     Application এর প্রতিটা স্তর থেকেই প্রপস ড্রিলিং সমাধানে সহয়তা করে।</p>
             </div>
+            <div className='container border border-mute text-secondary ps-3 my-4 text-start'>
+                <h6 className='fw-bold pt-3'>What is Semantic tag? </h6>
+                <p>Semantic tag উপাদানের উদ্দেশ্য নির্ধারণ করে। 
+                    এর উপাদানগুলো মানুষ ও মেশিন যাতে সহজে বুঝতে পারে তা  স্পষ্টভাবে বণর্না করা৷ 
+                    এটি ব্রাউজারে বিষয়বস্তুর অর্থ বুঝাতে সাহায্য করে। এটি অতিরিক্ত স্তরের স্পষ্টতা প্রদান করে। 
+                    Semantic tag প্রয়োজনীয় তথ্য দ্রুত বের করতে সহয়তা করে।</p>
+            </div>
         </div>
     );
 };
