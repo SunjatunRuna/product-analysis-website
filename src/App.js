69e5ece0379754b07a754724a098a4e35bb0e5c6
SunjatunRuna <sunjatuna@gmail.com>
2022-04-05 20:01:06 +0600

    sixth commit

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import About from './components/About/About';
 import Blogs from './components/Blogs/Blogs';
 import Dashboard from './components/Dashboard/Dashboard';
+import Error from './components/Error/Error';
 import Header from './components/Header/Header';
 import Home from './components/Home/Home';
 import Reviews from './components/Reviews/Reviews';
@@ -18,6 +19,7 @@ function App() {
         <Route path="/dashboard" element={<Dashboard />} />
         <Route path="/blogs" element={<Blogs />} />
         <Route path="/about" element={<About />} />
+        <Route path="*" element={<Error />} />
       </Routes>
     </div>
   );
---
+++ src/components/Error/Error.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Error = () => {
+    return (
+        <div className='my-5'>
+            <h1 className='mt-5 pt-5 text-warning'>404</h1>
+            <h3 className='text-danger'>Opps!! Something Went Wrong</h3>
+        </div>
+    );
+};
+
+export default Error;
