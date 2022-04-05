9e080a4da7884c5ecfd9e7686fc10059aecd9610
SunjatunRuna <sunjatuna@gmail.com>
2022-04-05 19:02:05 +0600

    fourth commit

---
+++ src/components/Chart/Chart.js
@@ -0,0 +1,66 @@
+import React from 'react';
+import { Line, LineChart, XAxis, YAxis } from 'recharts';
+
+const Chart = () => {
+    const data = [
+    {
+        "month": "Mar",
+        "investment": 100000,
+        "sell": 241,
+        "revenue": 10401
+    },
+    {
+        "month": "Apr",
+        "investment": 200000,
+        "sell": 423,
+        "revenue": 24500
+    },
+    {
+        "month": "May",
+        "investment": 500000,
+        "sell": 726,
+        "revenue": 67010
+    },
+    {
+        "month": "Jun",
+        "investment": 500000,
+        "sell": 529,
+        "revenue": 40405
+    },
+    {
+        "month": "Jul",
+        "investment": 600000,
+        "sell": 601,
+        "revenue": 50900
+    },
+    {
+        "month": "Aug",
+        "investment": 700000,
+        "sell": 670,
+        "revenue": 61000
+    }
+]
+    return (
+        <div className='container row'>
+            <div className='col-lg-6'>
+            <LineChart width={500} height={500} data={data}>
+                <Line dataKey={"investment"} stroke="#8884d8"></Line>
+                <XAxis dataKey={"month"}></XAxis>
+                <YAxis></YAxis>
+
+            </LineChart>
+            </div>
+            <div className='col-lg-6'>
+            <LineChart width={500} height={500} data={data}>
+                <Line dataKey={"investment"} stroke="#8884d8"></Line>
+                <XAxis dataKey={"month"}></XAxis>
+                <XAxis dataKey={"revenue"}></XAxis>
+                <YAxis></YAxis>
+
+            </LineChart>
+            </div>
+        </div>
+    );
+};
+
+export default Chart;
--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import Chart from '../Chart/Chart';
 
 const Dashboard = () => {
     return (
-        <div>
-            Dashboard page
+        <div className='container'>
+            <Chart></Chart>
         </div>
     );
 };
