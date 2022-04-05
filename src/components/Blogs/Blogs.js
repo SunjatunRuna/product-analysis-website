2f47fb88962d46d6542cb2e9c27d840eef90185a
SunjatunRuna <sunjatuna@gmail.com>
2022-04-05 20:36:23 +0600

    blog page update

--- src/components/Blogs/Blogs.js
+++ src/components/Blogs/Blogs.js
@@ -2,13 +2,15 @@ import React from 'react';
 
 const Blogs = () => {
     return (
-        <div>
-            <h5>What is Context API</h5>
-            <p>Context api হচ্ছে রিয়েক্টের স্ট্রাকচার। 
-                এটি এমন একটি উপায় যাতে কোনো রকম props পাস না করে উপাদানগুলোর মধ্যে মান সরবরাহ করা
-                 যায়। এটি react app এ গ্লোবাল ভেরিয়েবল তৈরীর একটি কার্যকরী উপায়। 
-                 এর মাধ্যমে সহজে ডাটা আদান প্রদান করা যায়। 
-                 Application এর প্রতিটা স্তর থেকেই প্রপস ড্রিলিং সমাধানে সহয়তা করে।</p>
+        <div className='container'>
+            <div className='container border border-mute text-secondary ps-3 text-start'>
+                <h6 className='fw-bold pt-3'>What is Context API?</h6>
+                <p>Context api হচ্ছে রিয়েক্টের স্ট্রাকচার।
+                    এটি এমন একটি উপায় যাতে কোনো রকম props পাস না করে উপাদানগুলোর মধ্যে মান সরবরাহ করা
+                    যায়। এটি react app এ গ্লোবাল ভেরিয়েবল তৈরীর একটি কার্যকরী উপায়।
+                    এর মাধ্যমে সহজে ডাটা আদান প্রদান করা যায়।
+                    Application এর প্রতিটা স্তর থেকেই প্রপস ড্রিলিং সমাধানে সহয়তা করে।</p>
+            </div>
         </div>
     );
 };
